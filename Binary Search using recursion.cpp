4db86ffb1d4e490b2489f5a470ed6204fa735fe4
Raghav Bhandari <raghavxb@gmail.com>
2018-02-21 19:50:18 -0800

    Update Binary Search using recursion.cpp

--- Binary Search using recursion.cpp
+++ Binary Search using recursion.cpp
@@ -1,15 +1,15 @@
-bin_search(int a[],int l,int r,int k)
+int binarySearch(int arr[], int l, int r, int x)
 {
-    if(l<=r)
-    {
-      int m=l+(l-r)/2;
-      if(a[m]==k)
-      return m;
-      if(a[m]<k)
-      return bin_search(a,mid+1,r,k);
-      else 
-      return bin_search(a,l,mid-1,k);
-     }
-     return -1;
- }
- 
+   if (r >= l)
+   {
+        int mid = l + (r - l)/2;
+        if (arr[mid] == x)  
+            return mid;
+
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid-1, x);
+
+        return binarySearch(arr, mid+1, r, x);
+   }
+   return -1;
+}
